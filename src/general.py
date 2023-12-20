6ec4cc280bb119c27468cecc6e62cf1e8b6bd69d
yue.zhang <zhangyue9306@gmail.com>
2023-12-20 14:31:13 +0800

    add sp500 historical ohlc

--- src/general.py
+++ src/general.py
@@ -9,15 +9,12 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-
 _downloader = DataDownloader()
 
 
 def download_sp500_key_metrics(period: str, limit: int, batch_size: int = 10):
-    _sleep_seconds = 40
-    _path = os.path.join(shared.PROJECT_DIR, 'artifacts', 'sp500_stocks.csv')
-    sp500 = pd.read_csv(_path)
-    sp500_tickers = sp500['Symbol'].tolist()
+    _sleep_seconds = 30
+    sp500_tickers = _load_sp500_tickers()
     cnt = 0
     batch_cnt = len(sp500_tickers) // batch_size + (1 if len(sp500_tickers) % batch_size else 0)
     for tickers in tqdm(_batch_generator(sp500_tickers, batch_size), total=batch_cnt):
@@ -32,10 +29,32 @@ def download_sp500_key_metrics(period: str, limit: int, batch_size: int = 10):
             time.sleep(_sleep_seconds)
 
 
+def download_sp500_ohlc(period: str = None, start_date: str = None, end_date: str = None, save_file: str = None):
+    sp500_tickers = _load_sp500_tickers()
+    df = _downloader.batch_fetch_tickers_ohlc(
+        tickers=sp500_tickers,
+        period=period,
+        start_date=start_date,
+        end_date=end_date)
+    if save_file:
+        _save_path = os.path.join(shared.PROJECT_DIR, 'artifacts', save_file)
+        df.to_csv(_save_path, index=False)
+        print(f"Data saved to {_save_path}")
+    return df
+
+
 def _batch_generator(lst: List, batch_size: int):
     for i in range(0, len(lst), batch_size):
         yield lst[i:i + batch_size]
 
 
+def _load_sp500_tickers() -> List:
+    _path = os.path.join(shared.PROJECT_DIR, 'artifacts', 'sp500_stocks.csv')
+    sp500 = pd.read_csv(_path)
+    return sp500['Symbol'].tolist()
+
+
 if __name__ == '__main__':
-    download_sp500_key_metrics(period='annual', limit=1000)
+    # download_sp500_key_metrics(period='annual', limit=1000)
+    data = download_sp500_ohlc(period='max', save_file='sp500_ohlc.csv')
+    print(data.head())
