c517c7f008962d4b5bcab6d95af0bfa33bde07f0
yue.zhang <zhangyue9306@gmail.com>
2024-01-12 18:14:07 +0800

    add cashflow growth fetch

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -139,6 +139,26 @@ class DataDownloader(object):
             df = pd.read_csv(_path)
         return df
 
+    def fetch_cashflow_growth(self, ticker: str, period: str, limit: int, refresh=False):
+        """
+        Reference to: https://site.financialmodelingprep.com/developer/docs#cashflow-growth-statement-analysis
+        :param ticker: e.g. AAPL
+        :param period: annual or quarter
+        :param limit: number of entries to fetch
+        :param refresh: refresh artifacts data
+        :return:
+        """
+        _api_path = "cash-flow-statement-growth"
+        _path = os.path.join(shared.PROJECT_DIR, 'artifacts', ticker.lower(), period, 'cashflow_growth.csv')
+        _check_or_create_directory(_path)
+        if refresh:
+            df = self._fetch_data_from_api(_api_path, ticker, period, limit)
+            df.to_csv(_path, index=False)
+            logging.info(f"Successfully fetched {_api_path} for {ticker}")
+        else:
+            df = pd.read_csv(_path)
+        return df
+
     def batch_fetch(self,
                     func: Callable,
                     tickers: List[str],
@@ -266,7 +286,7 @@ def _check_or_create_directory(path):
 
 if __name__ == '__main__':
     loader = DataDownloader()
-    data = loader.fetch_ratios(
+    data = loader.fetch_cashflow_growth(
         ticker='tsla',
         period='annual',
         limit=1000,
