998a5c4824370ccd7bfef4c175365500ccbeb0f8
yue.zhang <zhangyue9306@gmail.com>
2024-01-14 13:34:54 +0800

    balance sheet annual growth

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -179,6 +179,26 @@ class DataDownloader(object):
             df = pd.read_csv(_path)
         return df
 
+    def fetch_balance_sheet_growth(self, ticker: str, period: str, limit: int, refresh=False):
+        """
+        Reference to: https://site.financialmodelingprep.com/developer/docs#balance-sheet-growth-statement-analysis
+        :param ticker: e.g. AAPL
+        :param period: annual or quarter
+        :param limit: number of entries to fetch
+        :param refresh: refresh artifacts data
+        :return:
+        """
+        _api_path = "balance-sheet-statement-growth"
+        _path = os.path.join(shared.PROJECT_DIR, 'artifacts', ticker.lower(), period, 'balance_sheet_growth.csv')
+        _check_or_create_directory(_path)
+        if refresh:
+            df = self._fetch_data_from_api(_api_path, ticker, period, limit)
+            df.to_csv(_path, index=False)
+            logging.info(f"Successfully fetched {_api_path} for {ticker}")
+        else:
+            df = pd.read_csv(_path)
+        return df
+
     def batch_fetch(self,
                     func: Callable,
                     tickers: List[str],
@@ -306,7 +326,7 @@ def _check_or_create_directory(path):
 
 if __name__ == '__main__':
     loader = DataDownloader()
-    data = loader.fetch_income_growth(
+    data = loader.fetch_balance_sheet_growth(
         ticker='tsla',
         period='annual',
         limit=1000,
